d73e4c1b82e7d67707f4a383523bd1a0eba6a0ff
Chisom <chisomchinwendu18gmail.com>
2022-09-22 08:23:08 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+/**
+* print_rev - prints a string, followed by a new line.
+* @s: input string to print.
+*/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
