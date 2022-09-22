6b3b67a84c1f8773a476fd583b666f5b5934f412
Chisom <chisomchinwendu18gmail.com>
2022-09-22 08:09:27 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - to swap an int
+* @a: an int pointer
+* @b: an int pointer
+* Return: Always 0
+*/
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	int c = *b;
+
+	*a = c;
+	*b = i;
+}
